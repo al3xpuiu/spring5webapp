c7e8acdd28980e3a292d9c41b36cf71733114bdd
Loky <puiualexandru87@yahoo.com>
2018-07-30 19:58:41 +0200

    Added models. repositories and bootstrap

---
+++ src/main/java/com/bishoptod3/spring5webapp/model/repositories/PublisherRepository.java
@@ -0,0 +1,11 @@
+package com.bishoptod3.spring5webapp.model.repositories;
+
+import com.bishoptod3.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Loky on 30/07/2018.
+ */
+public interface PublisherRepository extends CrudRepository <Publisher, Long> {
+
+}
